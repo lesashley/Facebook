424b805c213e4c6b8019146ffb2c386a303d3a7c
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-10 14:14:51 -0500

    Login terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
-var users = [{email : "lesashley56@gmail.com",contrasena : "123456"},
-  {email : "leslie_96_2@hotmail.com", contrasena : "987654"}];
-
+var users = [{nombre: "Ashley", email : "lesashley56@gmail.com",contrasena :"leslie"},
+  {nombre: "Leslie", email : "leslie_96_2@hotmail.com", contrasena : "987654"},
+  {nombre: "Joel", email : "joel_03@hotmail.com", contrasena : "fabian"}];
+//users.forEach(function (e) {
+  //alert(e.email);
+//})
   document.getElementById("enviar").addEventListener("click",function (e) {
     var usuarios = document.getElementById("usuario").value;
     var contrasena = document.getElementById("contrasena").value;
@@ -16,6 +19,7 @@ var users = [{email : "lesashley56@gmail.com",contrasena : "123456"},
     	nombre.innerText=localStorage.getItem("nombre");
     }*/
     //-------
+    var expreEmail = /\S+@\S+\.\S+/;
     if(usuarios == "" && contrasena == ""){
       v1.style.display = "block";
       v2.style.display = "block";}
@@ -28,20 +32,30 @@ var users = [{email : "lesashley56@gmail.com",contrasena : "123456"},
       v2.style.display = "block";
     }
     else if (usuarios != "" && contrasena != ""){
-      var valido = false;
-      v1.style.display = "none";
+      var nombre;
+      v1.style.display = "block";
       v2.style.display = "none";
-      users.forEach(function(e){
-         if(e.email == usuarios && e.contrasena == contrasena){
-           valido = true;}
-        else {
-           valido = false; }
-       });
-       if(valido == true){
-         alert("Datos correctos");
-         window.location = "muro.html";}
-       else{alert("Datos incorrectos");}
-    }
 
+      users.forEach(function(e){
+        if (e.email != usuarios) {
+         v1.innerHTML = "Correo no válido";
+         return false;
+        }
+
+        else if (e.email == usuarios && e.contrasena != contrasena) {
+          v1.style.display = "none";
+          v2.style.display = "block";
+          v2.innerHTML = "Contraseña no válida";
+          return false;
+        }
+        else {
+         v1.style.display = "none";
+         v2.style.display = "none";
+         nombre = e.nombre;
+         alert("Bienvenido(a) a Facebook " + nombre);
+         window.location = "muro.html" + "?nombre=" +nombre;
+         return false;}
+        });
+    }
     e.preventDefault();
   });
--- assets/js/muro.js
+++ assets/js/muro.js
@@ -14,9 +14,39 @@ var data = new Datos();
 var botonPublicar = document.getElementById("enviar");
 botonPublicar.addEventListener("click", function(e){
     var texto = document.getElementById("texto").value;
-    var show = document.getElementById("mostrar");
-    show.innerHTML = "<div class='publicacion'><p>" + texto +"</p></div>";
-    data.agregar(texto);
+    var show = document.getElementById("resultado");
+    //show.innerHTML = "<div class='publicacion'><p>" + texto +"</p></div>";
+    //data.agregar(texto);
     //data.show();
+    show.appendChild(createPost(texto));
+    data.agregar(texto);
     e.preventDefault();
 })
+
+function createPost(contenido) {
+  var post = document.createElement('div');
+  var texto = document.createElement('p');
+  texto.innerHTML = contenido;
+  var eliminar = document.createElement('a');
+  eliminar.setAttribute('href', "#");
+  eliminar.innerHTML = "Eliminar";
+  var espacio = document.createElement('br');
+  var editar = document.createElement('a');
+  editar.setAttribute('href', "#");
+  editar.innerHTML = " Editar";
+  eliminar.addEventListener('click',function(e) {
+    e.preventDefault();
+    var postParent = e.target.parent; // Devuelve el padre
+  });
+  editar.addEventListener('click',function (e) {
+    e.preventDefault();
+    document.getElementById("resultado").value=contenido;
+  });
+  post.appendChild(texto);
+  post.appendChild(eliminar);
+  post.appendChild(espacio);
+  post.appendChild(editar);
+  return post;
+}
+//var resultado = document.getElementById('resultado');
+//resultado.appendChild(createPost("Gian"));
